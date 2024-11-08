bc96b20cd4b70a72ef04e42adc0d5fd89062b6b1
sago35 <sago35@gmail.com>
2024-11-08 09:23:43 +0900

    zero-kb02: change to go back to the original display after showing the layer

--- zero-kb02/firmware/main.go
+++ zero-kb02/firmware/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"errors"
 	"image/color"
 	"log"
 	"machine"
@@ -75,6 +76,7 @@ func run() error {
 		Rotation: drivers.Rotation180,
 	})
 	display.ClearDisplay()
+	displayBuffer := NewDisplayBuffer(display.Size())
 
 	var changed volatile.Register8
 	changed.Set(0)
@@ -308,6 +310,23 @@ func run() error {
 		}
 
 		if cnt%32 == 16 {
+			pixel := displayBuffer.GetPixel(dispx, dispy)
+			c := textWhite
+			if pixel {
+				c = textBlack
+			}
+			displayBuffer.SetPixel(dispx, dispy, c)
+			dispx += deltaX
+			dispy += deltaY
+
+			if dispx == 0 || dispx == 127 {
+				deltaX = -deltaX
+			}
+
+			if dispy == 0 || dispy == 63 {
+				deltaY = -deltaY
+			}
+
 			switch displayShowing {
 			case LAYER:
 				if displayFrame == 0 {
@@ -320,27 +339,10 @@ func run() error {
 					display.Display()
 					displayShowing = SCREENSAVER
 				}
-				break
 			case SCREENSAVER:
-				pixel := display.GetPixel(dispx, dispy)
-				c := textWhite
-				if pixel {
-					c = textBlack
-				}
-				display.SetPixel(dispx, dispy, c)
+				display.SetBuffer(displayBuffer.GetBuffer())
 				display.Display()
 
-				dispx += deltaX
-				dispy += deltaY
-
-				if dispx == 0 || dispx == 127 {
-					deltaX = -deltaX
-				}
-
-				if dispy == 0 || dispy == 63 {
-					deltaY = -deltaY
-				}
-				break
 			}
 			displayFrame++
 		}
@@ -350,3 +352,63 @@ func run() error {
 
 	return nil
 }
+
+type DisplayBuffer struct {
+	buffer []byte
+	width  int16
+	height int16
+}
+
+func NewDisplayBuffer(width, height int16) *DisplayBuffer {
+	return &DisplayBuffer{
+		buffer: make([]byte, width*height/8),
+		width:  width,
+		height: height,
+	}
+}
+
+func (d DisplayBuffer) Size() (x, y int16) {
+	return d.width, d.height
+}
+
+func (d *DisplayBuffer) SetPixel(x, y int16, c color.RGBA) {
+	if x < 0 || x >= d.width || y < 0 || y >= d.height {
+		return
+	}
+	byteIndex := x + (y/8)*d.width
+	if c.R != 0 || c.G != 0 || c.B != 0 {
+		d.buffer[byteIndex] |= 1 << uint8(y%8)
+	} else {
+		d.buffer[byteIndex] &^= 1 << uint8(y%8)
+	}
+}
+
+func (d DisplayBuffer) Display() error {
+	return nil
+}
+
+func (d *DisplayBuffer) GetPixel(x int16, y int16) bool {
+	if x < 0 || x >= d.width || y < 0 || y >= d.height {
+		return false
+	}
+	byteIndex := x + (y/8)*d.width
+	return (d.buffer[byteIndex] >> uint8(y%8) & 0x1) == 1
+}
+
+func (d *DisplayBuffer) SetBuffer(buffer []byte) error {
+	if len(buffer) != len(d.buffer) {
+		return errBufferSize
+	}
+	for i := 0; i < len(d.buffer); i++ {
+		d.buffer[i] = buffer[i]
+	}
+	return nil
+}
+
+func (d *DisplayBuffer) GetBuffer() []byte {
+	return d.buffer
+}
+
+var (
+	errBufferSize = errors.New("invalid size buffer")
+)
